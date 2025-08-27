94bf1925c06fc952e0faedd7d22876f4d3240fc4
jeruin-dev <jeruinpaulinorosa.dev@gmail.com>
2025-08-26 21:02:41 -0400

    Primer commint

---
+++ build/CMakeFiles/AngelEngine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AngelEngine.dir/link.d"
+  "AngelEngine"
+  "AngelEngine.pdb"
+  "CMakeFiles/AngelEngine.dir/main.cpp.o"
+  "CMakeFiles/AngelEngine.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AngelEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/AngelEngine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for AngelEngine.
+# This may be replaced when dependencies are built.
