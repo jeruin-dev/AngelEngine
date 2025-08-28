c05f8e23f88e8b4a1b19ed25741efd0ce9c8aec1
jeruin-dev <jeruinpaulinorosa.dev@gmail.com>
2025-08-28 15:21:20 -0400

    add AngelEngine

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 class Pkg(ConanFile):
-    name = "myapp"
+    name = "AngelEngine"
     version = "0.1"
     settings = "os", "compiler", "build_type", "arch"
     requires = ["fmt/11.2.0"]  # ejemplo
@@ -11,6 +11,8 @@ class Pkg(ConanFile):
     def layout(self):
         cmake_layout(self)
 
+    def requirements(self):
+        self.requires("angelscript/2.38.0")
 
     def generate(self):
         tc = CMakeToolchain(self)
